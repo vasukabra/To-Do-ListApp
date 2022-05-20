85a832b3fac36c98d1a7a44b988e1d2ec04ec1ec
Vasu <kabravasu@gmail.com>
2022-05-20 23:38:37 +0530

    Final Version

---
+++ app/src/main/java/app/web/thevasukabra/todolist/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package app.web.thevasukabra.todolist;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
